9b5f853a5c9df902cae403d5aac79dd3a10770fd
Digovil <dilangvidal@gmail.com>
2025-01-02 18:22:22 -0500

    group queries

--- car_rental_db/backup/2025_01_02_0001_car_rental_db.sql
+++ car_rental_db/backup/2025_01_02_0001_car_rental_db.sql
@@ -1,288 +1,142 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : DESKTOP-CBA25NH
- Source Server Type    : SQL Server
- Source Server Version : 16001000 (16.00.1000)
- Source Host           : DESKTOP-CBA25NH:1433
- Source Catalog        : CovisionDB
- Source Schema         : dbo
-
- Target Server Type    : SQL Server
- Target Server Version : 16001000 (16.00.1000)
- File Encoding         : 65001
-
- Date: 02/01/2025 00:24:13
-*/
-
-
--- ----------------------------
--- Table structure for ALQUILER
--- ----------------------------
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[ALQUILER]') AND type IN ('U'))
-	DROP TABLE [dbo].[ALQUILER]
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'CovisionDB')
+BEGIN
+    CREATE DATABASE CovisionDB;
+END;
 GO
 
-CREATE TABLE [dbo].[ALQUILER] (
-  [ID_ALQUILER] int  IDENTITY(1,1) NOT NULL,
-  [FECHA] date  NOT NULL,
-  [TIEMPO] int  NOT NULL,
-  [VALORTOTAL] decimal(18,2)  NOT NULL,
-  [SALDO] decimal(18,2)  NOT NULL,
-  [ABONO_INICIAL] decimal(18,2)  NOT NULL,
-  [DEVUELTO] bit DEFAULT 0 NOT NULL,
-  [ID_CLIENTE] int  NOT NULL,
-  [ID_CARRO] int  NOT NULL,
-  [CREATED_AT] datetime2(7) DEFAULT getdate() NULL,
-  [UPDATED_AT] datetime2(7) DEFAULT getdate() NULL
-)
-GO
-
-ALTER TABLE [dbo].[ALQUILER] SET (LOCK_ESCALATION = TABLE)
-GO
-
-
--- ----------------------------
--- Records of ALQUILER
--- ----------------------------
-BEGIN TRANSACTION
-GO
-
-SET IDENTITY_INSERT [dbo].[ALQUILER] ON
-GO
-
-INSERT INTO [dbo].[ALQUILER] ([ID_ALQUILER], [FECHA], [TIEMPO], [VALORTOTAL], [SALDO], [ABONO_INICIAL], [DEVUELTO], [ID_CLIENTE], [ID_CARRO], [CREATED_AT], [UPDATED_AT]) VALUES (N'1', N'2025-01-02', N'5', N'750000.00', N'300000.00', N'450000.00', N'0', N'1', N'1', N'2025-01-01 14:12:16.3666667', N'2025-01-01 14:12:16.3666667'), (N'2', N'2025-01-03', N'1', N'600000.00', N'200000.00', N'400000.00', N'1', N'2', N'2', N'2025-01-01 14:12:16.3666667', N'2025-01-01 14:12:16.3666667'), (N'3', N'2025-01-05', N'7', N'1400000.00', N'500000.00', N'900000.00', N'0', N'3', N'1', N'2025-01-01 14:12:16.3666667', N'2025-01-01 14:12:16.3666667')
-GO
-
-SET IDENTITY_INSERT [dbo].[ALQUILER] OFF
-GO
-
-COMMIT
-GO
-
-
--- ----------------------------
--- Table structure for CARRO
--- ----------------------------
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[CARRO]') AND type IN ('U'))
-	DROP TABLE [dbo].[CARRO]
-GO
-
-CREATE TABLE [dbo].[CARRO] (
-  [ID_CARRO] int  IDENTITY(1,1) NOT NULL,
-  [PLACA] nvarchar(10) COLLATE Modern_Spanish_CI_AS  NOT NULL,
-  [MARCA] nvarchar(50) COLLATE Modern_Spanish_CI_AS  NOT NULL,
-  [MODELO] nvarchar(50) COLLATE Modern_Spanish_CI_AS  NOT NULL,
-  [COSTO] decimal(18,2)  NOT NULL,
-  [DISPONIBLE] bit DEFAULT 1 NOT NULL,
-  [CREATED_AT] datetime2(7) DEFAULT getdate() NULL,
-  [UPDATED_AT] datetime2(7) DEFAULT getdate() NULL
-)
-GO
-
-ALTER TABLE [dbo].[CARRO] SET (LOCK_ESCALATION = TABLE)
-GO
-
-
--- ----------------------------
--- Records of CARRO
--- ----------------------------
-BEGIN TRANSACTION
-GO
-
-SET IDENTITY_INSERT [dbo].[CARRO] ON
-GO
-
-INSERT INTO [dbo].[CARRO] ([ID_CARRO], [PLACA], [MARCA], [MODELO], [COSTO], [DISPONIBLE], [CREATED_AT], [UPDATED_AT]) VALUES (N'1', N'ABC123', N'Toyota', N'Corolla', N'150000.00', N'1', N'2025-01-01 14:12:16.3633333', N'2025-01-01 14:12:16.3633333'), (N'2', N'DEF456', N'Mazda', N'CX-5', N'250000.00', N'1', N'2025-01-01 14:12:16.3633333', N'2025-01-01 14:12:16.3633333'), (N'3', N'GHI789', N'Ford', N'Focus', N'200000.00', N'0', N'2025-01-01 14:12:16.3633333', N'2025-01-01 14:12:16.3633333')
-GO
-
-SET IDENTITY_INSERT [dbo].[CARRO] OFF
-GO
-
-COMMIT
-GO
-
-
--- ----------------------------
--- Table structure for CLIENTE
--- ----------------------------
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[CLIENTE]') AND type IN ('U'))
-	DROP TABLE [dbo].[CLIENTE]
-GO
-
-CREATE TABLE [dbo].[CLIENTE] (
-  [ID_CLIENTE] int  IDENTITY(1,1) NOT NULL,
-  [CEDULA] nvarchar(20) COLLATE Modern_Spanish_CI_AS  NOT NULL,
-  [NOMBRE] nvarchar(100) COLLATE Modern_Spanish_CI_AS  NOT NULL,
-  [TELEFONO1] nvarchar(15) COLLATE Modern_Spanish_CI_AS  NULL,
-  [TELEFONO2] nvarchar(15) COLLATE Modern_Spanish_CI_AS  NULL,
-  [CREATED_AT] datetime2(7) DEFAULT getdate() NULL,
-  [UPDATED_AT] datetime2(7) DEFAULT getdate() NULL
-)
-GO
-
-ALTER TABLE [dbo].[CLIENTE] SET (LOCK_ESCALATION = TABLE)
-GO
-
-
--- ----------------------------
--- Records of CLIENTE
--- ----------------------------
-BEGIN TRANSACTION
-GO
-
-SET IDENTITY_INSERT [dbo].[CLIENTE] ON
-GO
-
-INSERT INTO [dbo].[CLIENTE] ([ID_CLIENTE], [CEDULA], [NOMBRE], [TELEFONO1], [TELEFONO2], [CREATED_AT], [UPDATED_AT]) VALUES (N'1', N'1234567890', N'Juan Pérez', N'3001234567', N'3101234567', N'2025-01-01 14:12:16.3000000', N'2025-01-01 14:12:16.3000000'), (N'2', N'9876543210', N'María López', N'3209876543', NULL, N'2025-01-01 14:12:16.3000000', N'2025-01-01 14:12:16.3000000'), (N'3', N'4567891234', N'Carlos García', N'3014567890', N'3114567890', N'2025-01-01 14:12:16.3000000', N'2025-01-01 14:12:16.3000000'), (N'4', N'2139123012', N'Isaac Mendoza', N'3014123923', N'3232030233', N'2025-01-01 14:32:14.5033333', N'2025-01-01 14:32:14.5033333')
-GO
-
-SET IDENTITY_INSERT [dbo].[CLIENTE] OFF
-GO
-
-COMMIT
-GO
-
-
--- ----------------------------
--- Table structure for PAGOS
--- ----------------------------
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[PAGOS]') AND type IN ('U'))
-	DROP TABLE [dbo].[PAGOS]
-GO
-
-CREATE TABLE [dbo].[PAGOS] (
-  [ID_PAGO] int  IDENTITY(1,1) NOT NULL,
-  [FECHA] date  NOT NULL,
-  [VALOR] decimal(18,2)  NOT NULL,
-  [ID_ALQUILER] int  NOT NULL,
-  [CREATED_AT] datetime2(7) DEFAULT getdate() NULL,
-  [UPDATED_AT] datetime2(7) DEFAULT getdate() NULL
-)
-GO
-
-ALTER TABLE [dbo].[PAGOS] SET (LOCK_ESCALATION = TABLE)
-GO
-
-
--- ----------------------------
--- Records of PAGOS
--- ----------------------------
-BEGIN TRANSACTION
-GO
-
-SET IDENTITY_INSERT [dbo].[PAGOS] ON
-GO
-
-INSERT INTO [dbo].[PAGOS] ([ID_PAGO], [FECHA], [VALOR], [ID_ALQUILER], [CREATED_AT], [UPDATED_AT]) VALUES (N'1', N'2025-01-02', N'200000.00', N'1', N'2025-01-01 14:12:16.3700000', N'2025-01-01 14:12:16.3700000'), (N'2', N'2025-01-03', N'100000.00', N'1', N'2025-01-01 14:12:16.3700000', N'2025-01-01 14:12:16.3700000'), (N'3', N'2025-01-04', N'200000.00', N'2', N'2025-01-01 14:12:16.3700000', N'2025-01-01 14:12:16.3700000'), (N'4', N'2025-01-05', N'300000.00', N'3', N'2025-01-01 14:12:16.3700000', N'2025-01-01 14:12:16.3700000'), (N'5', N'2025-01-06', N'200000.00', N'3', N'2025-01-01 14:12:16.3700000', N'2025-01-01 14:12:16.3700000')
-GO
-
-SET IDENTITY_INSERT [dbo].[PAGOS] OFF
-GO
-
-COMMIT
-GO
-
-
--- ----------------------------
--- Auto increment value for ALQUILER
--- ----------------------------
-DBCC CHECKIDENT ('[dbo].[ALQUILER]', RESEED, 3)
-GO
-
-
--- ----------------------------
--- Primary Key structure for table ALQUILER
--- ----------------------------
-ALTER TABLE [dbo].[ALQUILER] ADD CONSTRAINT [PK__ALQUILER__4C7F0687E4BEF7EA] PRIMARY KEY CLUSTERED ([ID_ALQUILER])
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Auto increment value for CARRO
--- ----------------------------
-DBCC CHECKIDENT ('[dbo].[CARRO]', RESEED, 3)
-GO
-
-
--- ----------------------------
--- Uniques structure for table CARRO
--- ----------------------------
-ALTER TABLE [dbo].[CARRO] ADD CONSTRAINT [UQ__CARRO__E441AE008B9FEB32] UNIQUE NONCLUSTERED ([PLACA] ASC)
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Primary Key structure for table CARRO
--- ----------------------------
-ALTER TABLE [dbo].[CARRO] ADD CONSTRAINT [PK__CARRO__6F4C659FDA6B0BD4] PRIMARY KEY CLUSTERED ([ID_CARRO])
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Auto increment value for CLIENTE
--- ----------------------------
-DBCC CHECKIDENT ('[dbo].[CLIENTE]', RESEED, 4)
-GO
-
-
--- ----------------------------
--- Uniques structure for table CLIENTE
--- ----------------------------
-ALTER TABLE [dbo].[CLIENTE] ADD CONSTRAINT [UQ__CLIENTE__06BB844807FAC0FC] UNIQUE NONCLUSTERED ([CEDULA] ASC)
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Primary Key structure for table CLIENTE
--- ----------------------------
-ALTER TABLE [dbo].[CLIENTE] ADD CONSTRAINT [PK__CLIENTE__23A34130170693E9] PRIMARY KEY CLUSTERED ([ID_CLIENTE])
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Auto increment value for PAGOS
--- ----------------------------
-DBCC CHECKIDENT ('[dbo].[PAGOS]', RESEED, 5)
-GO
-
-
--- ----------------------------
--- Primary Key structure for table PAGOS
--- ----------------------------
-ALTER TABLE [dbo].[PAGOS] ADD CONSTRAINT [PK__PAGOS__B68D23DF5E428C1E] PRIMARY KEY CLUSTERED ([ID_PAGO])
-WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)  
-ON [PRIMARY]
-GO
-
-
--- ----------------------------
--- Foreign Keys structure for table ALQUILER
--- ----------------------------
-ALTER TABLE [dbo].[ALQUILER] ADD CONSTRAINT [FK_ALQUILER_CLIENTE] FOREIGN KEY ([ID_CLIENTE]) REFERENCES [dbo].[CLIENTE] ([ID_CLIENTE]) ON DELETE NO ACTION ON UPDATE NO ACTION
-GO
-
-ALTER TABLE [dbo].[ALQUILER] ADD CONSTRAINT [FK_ALQUILER_CARRO] FOREIGN KEY ([ID_CARRO]) REFERENCES [dbo].[CARRO] ([ID_CARRO]) ON DELETE NO ACTION ON UPDATE NO ACTION
-GO
-
-
--- ----------------------------
--- Foreign Keys structure for table PAGOS
--- ----------------------------
-ALTER TABLE [dbo].[PAGOS] ADD CONSTRAINT [FK_PAGOS_ALQUILER] FOREIGN KEY ([ID_ALQUILER]) REFERENCES [dbo].[ALQUILER] ([ID_ALQUILER]) ON DELETE NO ACTION ON UPDATE NO ACTION
-GO
-
-
+USE CovisionDB;
+
+-- Crear tabla CLIENTE si no existe
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CLIENTE]') AND type = N'U')
+BEGIN
+    CREATE TABLE CLIENTE (
+        ID_CLIENTE INT IDENTITY(1,1) PRIMARY KEY,
+        CEDULA NVARCHAR(20) NOT NULL UNIQUE,
+        NOMBRE NVARCHAR(100) NOT NULL,
+        TELEFONO1 NVARCHAR(15),
+        TELEFONO2 NVARCHAR(15),
+        CREATED_AT DATETIME2 DEFAULT GETDATE(),
+        UPDATED_AT DATETIME2 DEFAULT GETDATE()
+    );
+END;
+
+-- Crear tabla CARRO si no existe
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CARRO]') AND type = N'U')
+BEGIN
+    CREATE TABLE CARRO (
+        ID_CARRO INT IDENTITY(1,1) PRIMARY KEY,
+        PLACA NVARCHAR(10) NOT NULL UNIQUE,
+        MARCA NVARCHAR(50) NOT NULL,
+        MODELO NVARCHAR(50) NOT NULL,
+        COSTO DECIMAL(18, 2) NOT NULL,
+        DISPONIBLE BIT NOT NULL DEFAULT 1, -- Booleano
+        CREATED_AT DATETIME2 DEFAULT GETDATE(),
+        UPDATED_AT DATETIME2 DEFAULT GETDATE()
+    );
+END;
+
+-- Crear tabla ALQUILER si no existe
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ALQUILER]') AND type = N'U')
+BEGIN
+    CREATE TABLE ALQUILER (
+        ID_ALQUILER INT IDENTITY(1,1) PRIMARY KEY,
+        FECHA DATE NOT NULL,
+        TIEMPO INT NOT NULL, -- En días
+        VALORTOTAL DECIMAL(18, 2) NOT NULL,
+        SALDO DECIMAL(18, 2) NOT NULL,
+        ABONO_INICIAL DECIMAL(18, 2) NOT NULL,
+        DEVUELTO BIT NOT NULL DEFAULT 0, -- Booleano
+        ID_CLIENTE INT NOT NULL,
+        ID_CARRO INT NOT NULL,
+        CREATED_AT DATETIME2 DEFAULT GETDATE(),
+        UPDATED_AT DATETIME2 DEFAULT GETDATE()
+    );
+END;
+
+-- Crear tabla PAGOS si no existe
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PAGOS]') AND type = N'U')
+BEGIN
+    CREATE TABLE PAGOS (
+        ID_PAGO INT IDENTITY(1,1) PRIMARY KEY,
+        FECHA DATE NOT NULL,
+        VALOR DECIMAL(18, 2) NOT NULL,
+        ID_ALQUILER INT NOT NULL,
+        CREATED_AT DATETIME2 DEFAULT GETDATE(),
+        UPDATED_AT DATETIME2 DEFAULT GETDATE()
+    );
+END;
+
+
+
+-- Crear relaciones solo si no existen
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_ALQUILER_CLIENTE')
+BEGIN
+    ALTER TABLE ALQUILER
+    ADD CONSTRAINT FK_ALQUILER_CLIENTE FOREIGN KEY (ID_CLIENTE) REFERENCES CLIENTE(ID_CLIENTE);
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_ALQUILER_CARRO')
+BEGIN
+    ALTER TABLE ALQUILER
+    ADD CONSTRAINT FK_ALQUILER_CARRO FOREIGN KEY (ID_CARRO) REFERENCES CARRO(ID_CARRO);
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_PAGOS_ALQUILER')
+BEGIN
+    ALTER TABLE PAGOS
+    ADD CONSTRAINT FK_PAGOS_ALQUILER FOREIGN KEY (ID_ALQUILER) REFERENCES ALQUILER(ID_ALQUILER);
+END;
+
+
+
+-- Crear relaciones solo si no existen
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_ALQUILER_CLIENTE')
+BEGIN
+    ALTER TABLE ALQUILER
+    ADD CONSTRAINT FK_ALQUILER_CLIENTE FOREIGN KEY (ID_CLIENTE) REFERENCES CLIENTE(ID_CLIENTE);
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_ALQUILER_CARRO')
+BEGIN
+    ALTER TABLE ALQUILER
+    ADD CONSTRAINT FK_ALQUILER_CARRO FOREIGN KEY (ID_CARRO) REFERENCES CARRO(ID_CARRO);
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_PAGOS_ALQUILER')
+BEGIN
+    ALTER TABLE PAGOS
+    ADD CONSTRAINT FK_PAGOS_ALQUILER FOREIGN KEY (ID_ALQUILER) REFERENCES ALQUILER(ID_ALQUILER);
+END;
+
+-- Creando registros de CLIENTE
+INSERT INTO CLIENTE (CEDULA, NOMBRE, TELEFONO1, TELEFONO2)
+VALUES 
+('1234567890', 'Juan Pérez', '3001234567', '3101234567'),
+('9876543210', 'María López', '3209876543', NULL),
+('4567891234', 'Carlos García', '3014567890', '3114567890');
+
+-- Creando registros de CARRO
+INSERT INTO CARRO (PLACA, MARCA, MODELO, COSTO, DISPONIBLE)
+VALUES 
+('ABC123', 'Toyota', 'Corolla', 150000.00, 1),
+('DEF456', 'Mazda', 'CX-5', 250000.00, 1),
+('GHI789', 'Ford', 'Focus', 200000.00, 0);
+
+-- Creando registros de ALQUILER
+INSERT INTO ALQUILER (FECHA, TIEMPO, VALORTOTAL, SALDO, ABONO_INICIAL, DEVUELTO, ID_CLIENTE, ID_CARRO)
+VALUES 
+('2025-01-02', 5, 750000.00, 300000.00, 450000.00, 0, 1, 1),
+('2025-01-03', 1, 600000.00, 200000.00, 400000.00, 1, 2, 2),
+('2025-01-05', 7, 1400000.00, 500000.00, 900000.00, 0, 3, 1);
+
+-- Creando registros de PAGOS
+INSERT INTO PAGOS (FECHA, VALOR, ID_ALQUILER)
+VALUES 
+('2025-01-02', 200000.00, 1),
+('2025-01-03', 100000.00, 1),
+('2025-01-04', 200000.00, 2),
+('2025-01-05', 300000.00, 3),
+('2025-01-06', 200000.00, 3);
 
 
 
@@ -357,6 +211,7 @@ JOIN ALQUILER A ON C.ID_CLIENTE = A.ID_CLIENTE
 JOIN CARRO CA ON A.ID_CARRO = CA.ID_CARRO
 WHERE C.CEDULA = '4567891234'
 GROUP BY C.CEDULA, C.NOMBRE, CA.PLACA, CA.MODELO, CA.MARCA, CA.COSTO;
+
 
 
 
